28f94144e543a3aa118347d86242057b724e00f8
Ian Macalinao <me@ian.pw>
2015-06-21 01:50:31 -0700

    ensure token

--- commands/provision.js
+++ commands/provision.js
@@ -2,6 +2,9 @@ import machine from 'dockermachine';
 
 export default async function(req, res) {
   let token = req.body.token;
+  if (!token) {
+    return res.json({ message: 'No token RIP' });
+  }
   let name = req.body.name || 'mozart-' + Math.floor(Math.random() * 10000).toString();
   let machine = await spawnMachine(token, name);
   res.json({ machine });
