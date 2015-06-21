fac90baad774f2da2d963a5e9a77ebb476ad85ca
Ian Macalinao <me@ian.pw>
2015-06-21 01:08:07 -0700

    Mozart provisioning

--- commands/provision.js
+++ commands/provision.js
@@ -1,4 +1,16 @@
-import { exec } from 'child_process';
+import { spawn } from 'child_process';
 
 export default function(req, res) {
+  let token = req.body.token;
+  let name = req.body.name || 'mozart-' + Math.floor(Math.random() * 10000).toString();
+  let machine = spawnMachine(token, name);
+  machine.on('close', (code, signal) => {
+    res.json({
+      success: true
+    });
+  });
+}
+
+function spawnMachine(doToken, name) {
+  return spawn.call([this].concat(`docker-machine create -d digitalocean --digitalocean-access-token=${doToken} --digitalocean-size "1gb" ${name}`.split(' ')));
 }
