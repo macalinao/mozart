a7b53a42d2611101d5ff9c1d86c66c4a3d8f9a47
Ian Macalinao <me@ian.pw>
2015-06-21 01:25:40 -0700

    add compose

---
+++ commands/compose.js
@@ -0,0 +1,24 @@
+import { exec } from 'child-process-promise';
+import md5 from 'MD5';
+
+export default function(req, res) {
+  let repo = req.body.repo;
+  let path = req.body.path;
+  let compose = await spawnCompose(repo, branch);
+  res.json({
+    err: compose.results.stderr,
+    out: compose.results.stdout,
+    randy: compose.randy
+  });
+}
+
+async function spawnCompose(repo, branch) {
+  let randy = 'mozart-' + md5(repo + '/' + branch);
+  let cmds = [
+    `git clone -b ${branch} ${repo} ${randy}`,
+    `cd ${randy}`,
+    `docker-compose up -d`
+  ];
+  let results = await exec(cmds.join(';'));
+  return { results, randy };
+}
--- commands/provision.js
+++ commands/provision.js
@@ -1,16 +1,16 @@
-import { spawn } from 'child_process';
+import machine from 'dockermachine';
 
-export default function(req, res) {
+export default async function(req, res) {
   let token = req.body.token;
   let name = req.body.name || 'mozart-' + Math.floor(Math.random() * 10000).toString();
-  let machine = spawnMachine(token, name);
-  machine.on('close', (code, signal) => {
-    res.json({
-      success: true
-    });
-  });
+  let machine = await spawnMachine(token, name);
+  res.json({ name });
 }
 
-function spawnMachine(doToken, name) {
-  return spawn.call([this].concat(`docker-machine create -d digitalocean --digitalocean-access-token=${doToken} --digitalocean-size "1gb" ${name}`.split(' ')));
+async function spawnMachine(doToken, name) {
+  return machine.create(name, {
+    driver: 'digitalocean',
+    'digitalocean-access-token': doToken,
+    'digitalocean-size': '1gb'
+  });
 }
