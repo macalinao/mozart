53881ac129144fe58caf261d623b6dd67930a8e4
Ian Macalinao <me@ian.pw>
2015-06-21 00:57:13 -0700

    urlencode body parser

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 import provision from './commands/provision';
 import compose from './commands/compose';
 
 let app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.post('/provision', provision);
 app.post('/compose', compose);
