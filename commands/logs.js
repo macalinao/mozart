244cd770d5f58a40ba335ee3f71c217a5eaaed35
AnandkumarPatel <anandkumarpatel@gmail.com>
2015-06-21 10:35:11 -0700

    change to params

--- commands/logs.js
+++ commands/logs.js
@@ -2,7 +2,7 @@ import { db } from '../ghettodb';
 import fs from 'fs';
 
 export default async function(req, res) {
-  let id = req.param.id;
+  let id = req.params.id;
   if (!id) {
     return res.json({ message: 'No id RIP' });
   }
