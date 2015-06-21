39acbe46ebcf4c6da723102bf5b24322fee6223e
Ian Macalinao <me@ian.pw>
2015-06-21 00:55:03 -0700

    Comands

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 import express from 'express';
 
+import provision from './commands/provision';
+import compose from './commands/compose';
+
 let app = express();
+
+app.post('/provision', provision);
+app.post('/compose', compose);
 
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
